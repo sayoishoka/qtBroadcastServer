9cb8ede91f66ca92a7a9eac5f27408fedc65ab1b
幻夜空梦 <3413224303@qq.com>
2024-02-15 19:40:32 +0800

    主页设计基本完成

--- menu.cpp
+++ menu.cpp
@@ -8,6 +8,7 @@ Menu::Menu(QWidget *parent) :
 {
     ui->setupUi(this);
     init();
+    timerr();
     signal_slotConnection();
 }
 
@@ -25,6 +26,18 @@ void Menu::Open_Function(QTreeWidgetItem *item, int column)
     }else if (QString::localeAwareCompare(item->text(0),"主页")==0){
         ui->stackedWidget->setCurrentIndex(0);
     }
+}
+
+void Menu::timerr()
+{
+    QDateTime dateTime1 =QDateTime::currentDateTime();//获取系统时间
+    ui->time->setText(dateTime1.toString("hh:mm:ss"));
+    QTimer* timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, [=](){
+        QDateTime dateTime2 =QDateTime::currentDateTime();//获取当前系统时间
+        ui->time->setText(dateTime2.toString("hh:mm:ss"));
+    });//连接信号槽
+    timer->start(1000);//1s更新一次
 }
 
 void Menu::init()
@@ -56,6 +69,31 @@ void Menu::init()
 
         //设置多级菜单部件的最大宽度
         //        this->setMaximumWidth(350);
+
+        pMenuBar = new QMenuBar;
+
+        pMenuBar->setStyleSheet("QMenuBar{font-size:18px;height:65px;background-color:transparent;}"/*设置背景色，跟随背景色*/
+                               "QMenuBar::selected{background-color:transparent;}"/*设置菜单栏选中背景色*/
+                               "QMenuBar::item{widget:80px;font-size:30px;font-family:Microsoft YaHei;color:rgba(0,0,0,1);}"/*设置菜单栏字体为黑色，透明度为1（取值范围0.0-255）*/
+                                );
+        pMenuTest1 = new QMenu("用户名");
+//        pMenuTest1->setFixedHeight(300);//这样只把菜单变大了
+//        pMenuTest1->setMinimumWidth(150);//指定菜单的宽度
+        pMenuTest1->setStyleSheet("QMenu{background:rgba(255,255,255,1);border:none;font-size:16px;}"
+                                  "QMenu::item{padding:11px 32px;color:rgba(51,51,51,1);font-size:16px;}"
+                                  "QMenu::item:hover{background-color:#409CE1;}"
+                                  "QMenu::item:selected{background-color:#409CE1;}");
+        pActionTest1 = new QAction("个人信息修改");
+        pActionTest2 = new QAction("切换账号");
+        pActionTest3 = new QAction("登出");
+        list1.append(pActionTest1);
+        list1.append(pActionTest2);
+        list1.append(pActionTest3);
+        pMenuTest1->addActions(list1);
+        pMenuBar->addMenu(pMenuTest1);
+        pMenuBar->addSeparator();    //分隔栏
+        ui->horizontalLayout->addWidget(pMenuBar);
+
 }
 
 void Menu::signal_slotConnection()
--- menu.h
+++ menu.h
@@ -5,7 +5,9 @@
 
 #include <QWidget>
 #include <QTreeWidgetItem>
-
+#include <QMenuBar>
+#include <QDateTime>
+#include <QTimer>
 namespace Ui {
 class Menu;
 }
@@ -21,8 +23,20 @@ public:
 private slots:
     void Open_Function(QTreeWidgetItem *item, int column);
 private:
+    void timerr();//定时器
     void init();
     void signal_slotConnection();
+    //菜单栏
+    QMenuBar *pMenuBar;
+    //菜单项
+    QMenu *pMenuTest1;
+    //子菜单
+    QAction *pActionTest1;
+    QAction *pActionTest2;
+    QAction *pActionTest3;
+    QAction *pActionTest4;
+
+    QList<QAction*> list1;
     User_Manage *user_mange = new User_Manage();
     Ui::Menu *ui;
 };
